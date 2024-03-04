18615d5d75643454e9d498d2ba3a2ce238a5f8a6
robenend <roberaendale2@gmail.com>
2024-03-04 12:15:20 +0300

    first-build

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import ProductCard from "./components/ProductCard";
 import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/route";
+import { authOptions } from "./api/auth/authOptions";
 import image from "@/public/images/image.png";
 // import HeavyComponent from "./components/HeavyComponent";
 import dynamic from "next/dynamic";
